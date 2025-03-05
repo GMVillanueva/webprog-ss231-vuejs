059bf749c49a4aa2ece82c659d89155ae1a4f998
GMVillanueva <villanueva9012@gmail.com>
2025-03-05 22:54:43 +0800

    Added PersonalProfile

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,10 +2,14 @@
 
 <template>
   <h1>Food</h1>
-  <food-item/>
-  <food-item2/>
-  <food-item/>
-  <food-item2/>
+  <personal-profile/> <br>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/> <br>
+  <food-item2/> <br>
 </template>
 
-<style></style>
+<script></script>
+
+<style>
+</style>
--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -1,1 +1,22 @@
-
+<template>
+<div class="container">
+    <header>
+        <h1>My Personal Profile Webpage</h1>
+        </header>
+    <main>
+        <section class="main-content">
+                <div><br>About me</div>
+                <div><br>(Do not include birthdate / age / contact information)</div>
+                <div><br>Education/Achievements: Elementary and Senior High School Graduate</div>
+                <div><br>Course: Computer Science</div>
+                <div><br>IT Experience: Programming, FiveM Developer, and Habbo Retros developer</div>
+                <div><br>Hobbies & Interests: Gaming, Films, and Technologies</div>
+                <div><br>Goals in Life/Dream: To be successful</div>
+                <div><br>Picture Gallery</div>
+            </section>
+            aside class="sidebar"> 
+                </aside>
+            </main>
+        <footer> Asia Pacific College </footer>
+    </div>
+</template>
